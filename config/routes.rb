d7575e763031e083fb5871c9da7097754aedc8df
Samuel Stewart <samuelcstewart@icloud.com>
2015-10-27 23:01:35 +1300

    Change admin site title to Dogs on Beaches. Root route to admin dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :animals, only: [:index, :show]
   resources :reports, only: [:index, :show, :create]
-  resources :locations, only: [:index, :show]
+  resources :locations, only: [:index, :show] do
+    resources :dog_statuses, only: [:index, :show]
+  end
   resources :sync, only: [:index]
+  root to: "admin/dashboard#index"
 end
